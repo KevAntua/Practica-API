1e93a6ed813a7ad5a1d13f88589b9d190b59fb4f
KevAntua <65007807+KevAntua@users.noreply.github.com>
2022-01-24 12:32:58 -0600

    Add files via upload

--- mascotas-api/src/main/java/com/veterinaria/mascotas/entitys/Mascota.java
+++ mascotas-api/src/main/java/com/veterinaria/mascotas/entitys/Mascota.java
@@ -2,15 +2,19 @@ package com.veterinaria.mascotas.entitys;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 //Objeto Plano (propiedades y metodos de acceso)
 
 @Entity //Indica que es una entidad
 @Table(name="mascotas")
+@EntityListeners(AuditingEntityListener.class)
 public class Mascota {
     
 	@Id
